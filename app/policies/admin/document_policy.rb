38ebc058e21813d0ce27c49cbd5f183b4c932591
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-05 06:34:25 +0100

    add policy for admin::documents

---
+++ app/policies/admin/document_policy.rb
@@ -0,0 +1,15 @@
+class Admin::DocumentPolicy < ApplicationPolicy
+  def create?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
