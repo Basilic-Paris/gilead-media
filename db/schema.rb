37f3f522602fd05ea48c947529b38aeac62358d9
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-05 05:30:31 +0100

    run rails generate migration CreateOldPasswords

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_041907) do
+ActiveRecord::Schema.define(version: 2021_01_05_042935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "old_passwords", force: :cascade do |t|
+    t.string "encrypted_password", null: false
+    t.string "password_archivable_type", null: false
+    t.integer "password_archivable_id", null: false
+    t.string "password_salt"
+    t.datetime "created_at"
+    t.index ["password_archivable_type", "password_archivable_id"], name: "index_password_archivable"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
