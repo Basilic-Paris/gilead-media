337bc68e83376336cdb5ee5ae52919d3a8c5aebd
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-05 06:39:13 +0100

    add #validated? in document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,4 +3,8 @@ class Document < ApplicationRecord
 
   validates :title, presence: true
   validates :language, presence: true, inclusion: { in: LANGUAGES }
+
+  def validated?
+    validation_at.present?
+  end
 end
