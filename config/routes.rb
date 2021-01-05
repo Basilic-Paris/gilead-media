3691f40efb4d660a14a7b419001d14e6b20fbc68
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-05 06:40:05 +0100

    add route show for admin::documents

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
   authenticate :user, ->(user) { user.admin } do
     namespace :admin do
       resources :users, only: %i[index create]
-      resources :documents, only: %i[new create edit update]
+      resources :documents, only: %i[show new create edit update] do
+        patch :validate
+      end
     end
   end
 end
