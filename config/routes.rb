597dd6e7fbd77dace89930f0c2d7e7f46eb3b2c4
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-05 05:36:00 +0100

    add routes to authorize users to edit password using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do #as :user do
+    get 'users/edit', to: 'devise/registrations#edit', as: :edit_user_registration
+    put 'users', to: 'devise/registrations#update', as: :user_registration
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
