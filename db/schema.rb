b5209e537682f3aaa814e66f4ee0acdda177c7fa
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-05 05:20:15 +0100

    add password_changed_at and index on password_changed_at to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_035754) do
+ActiveRecord::Schema.define(version: 2021_01_05_041907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,9 @@ ActiveRecord::Schema.define(version: 2021_01_05_035754) do
     t.string "first_name"
     t.string "last_name"
     t.boolean "admin", default: false
+    t.datetime "password_changed_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["password_changed_at"], name: "index_users_on_password_changed_at"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
