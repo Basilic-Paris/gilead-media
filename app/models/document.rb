3aeb027db0aad81b5664cda6ac42558c945dd4fe
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-05 06:37:15 +0100

    set LANGUAGES constant in document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,6 @@
 class Document < ApplicationRecord
+  LANGUAGES = %w[FR EN]
+
   validates :title, presence: true
-  validates :language, presence: true, inclusion: { in: %w[FR EN] }
+  validates :language, presence: true, inclusion: { in: LANGUAGES }
 end
