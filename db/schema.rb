e6143e9ee391b37029a8d1feafc533dab29a0497
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-19 18:29:42 +0100

    destroy table folders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_171225) do
+ActiveRecord::Schema.define(version: 2021_01_19_172834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,12 +42,6 @@ ActiveRecord::Schema.define(version: 2021_01_19_171225) do
     t.text "usage"
     t.datetime "validation_at"
     t.string "format"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "folders", force: :cascade do |t|
-    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
