f47bf2296e17f1541c3eaf62ce400c3f45d17325
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-22 16:23:34 +0100

    add default_scope to folder model

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -3,4 +3,6 @@ class Folder < ApplicationRecord
   has_many :documents, through: :document_folders
 
   validates :title, presence: true, uniqueness: true
+
+  default_scope { order('lower(title)') }
 end
