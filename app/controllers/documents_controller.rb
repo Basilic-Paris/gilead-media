ae9656228cdf8344a75eb4739b84377764a1e6b0
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-28 23:19:21 +0100

    update action index in documents controller to display results depending on search add search params private method

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,4 @@
 class DocumentsController < ApplicationController
-
   def show
     @document = Document.find(params[:id])
     authorize @document
@@ -7,6 +6,36 @@ class DocumentsController < ApplicationController
 
   def index
     @documents = policy_scope(Document)
+    if search_params.present?
+      search_params.each do |key, value|
+        if key != "created_at"
+          @documents = @documents.advanced_search(key.to_sym, value)
+        else
+          @documents = @documents.created_in_day_range_around(value)
+        end
+      end
+    else
+      @documents
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
+  private
+
+  def search_params
+    search = Hash.new
+    search[:title] =  params[:title] if params[:title].present?
+    search[:format] = params[:fmt] if params[:fmt].present?
+    search[:language] = params[:language] if params[:language].present?
+    if params[:date].present?
+      if params[:date]["created_at(1i)"].present? && params[:date]["created_at(2i)"].present? && params[:date]["created_at(3i)"].present?
+        search[:created_at] = Date.new(params[:date]["created_at(1i)"].to_i, params[:date]["created_at(2i)"].to_i, params[:date]["created_at(3i)"].to_i)
+      end
+    end
+    return search
+  end
 end
