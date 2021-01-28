b4c768027c9392953201b68c2a27b1fc4eccc491
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-28 23:28:19 +0100

    remove connect() function from js search controller

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -3,10 +3,6 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "dropdownIcon", "moreOptions" ]
 
-  connect() {
-    console.log('Hello, Stimulus!')
-  }
-
   toggle() {
     this.dropdownIconTarget.classList.toggle('fa-sort-down');
     this.dropdownIconTarget.classList.toggle('fa-sort-up');
