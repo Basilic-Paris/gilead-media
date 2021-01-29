d9aeb4ed31cc435ecbbed207ea345672f71e9bd5
Alice Fabre <alice.fabre@hotmail.fr>
2021-01-29 17:52:46 +0100

    use simple_form_for in seach_form instead of text_field_tags and update search_params in controller accordingly

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -7,12 +7,8 @@ class DocumentsController < ApplicationController
   def index
     @documents = policy_scope(Document)
     if search_params.present?
-      search_params.each do |key, value|
-        if key != :created_at
-          @documents = @documents.advanced_search(key.to_sym, value)
-        else
-          @documents = @documents.created_in_days_range(value.first, value.last)
-        end
+      search_params.reject { |key, value| value.blank? }.each do |key, value|
+        key == :created_at ? @documents = @documents.created_in_days_range(value.first, value.last) : @documents = @documents.advanced_search(key, value)
       end
     else
       @documents
@@ -27,11 +23,6 @@ class DocumentsController < ApplicationController
   private
 
   def search_params
-    search = Hash.new
-    search[:title] =  params[:title] if params[:title].present?
-    search[:format] = params[:fmt] if params[:fmt].present?
-    search[:language] = params[:language] if params[:language].present?
-    search[:created_at] = params[:created_at].split(" au ").map { |date| date.to_date } if params[:created_at].present?
-    return search
+    params.fetch(:search, {}).permit(:title, :fmt, :language, :created_at)
   end
 end
