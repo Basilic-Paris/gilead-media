9828efbb20d3670cdb3bb38aaba6458dea215da4
Alice Fabre <alice.fabre@hotmail.fr>
2021-02-04 11:11:40 +0100

    add routes / policy / action in controller to send shared lists to contacts

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   end
   resources :shared_lists, only: %i[index show] do
     patch :add_contacts, on: :member
+    patch :send_to_contacts, on: :member
   end
 
   # -------- ADMIN ROUTES ---------
