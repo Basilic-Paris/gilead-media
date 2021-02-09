b4deeee17418091ff1aaf7ff53f058e25b74b64f
Alice Fabre <alice.fabre@hotmail.fr>
2021-02-09 18:32:26 +0100

    add routes / controllers / views for public namespace

---
+++ spec/requests/public/documents_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Public::Documents", type: :request do
+
+end
---
+++ spec/requests/public/folders_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Public::Folders", type: :request do
+
+end
