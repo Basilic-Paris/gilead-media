3274647b6fffc9cc71a817d54e83aba2ffa3fccd
Alice Fabre <51875307+Alice-F@users.noreply.github.com>
2021-02-10 15:15:33 +0100

    Revert "patch to remove shared lists form gilead-media-staging / will be reverted"

--- spec/factories/shared_documents.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :shared_document do
-    document { nil }
-    shared_list { nil }
-  end
-end
--- spec/factories/shared_folders.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :shared_folder do
-    folder { nil }
-    shared_list { nil }
-  end
-end
