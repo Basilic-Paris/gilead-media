3e5a6c9d98537f6a4e18dfc857defb9c584c5e0f
Alice Fabre <51875307+Alice-F@users.noreply.github.com>
2021-02-10 14:06:42 +0100

    Revert "Rename shared document in document shared list"

--- app/controllers/shared_documents_controller.rb
+++ app/controllers/shared_documents_controller.rb
@@ -1,10 +1,10 @@
-class DocumentSharedListsController < ApplicationController
+class SharedDocumentsController < ApplicationController
   def create
     @shared_list = SharedList.new
     @document = Document.find(params[:document_id]).decorate
-    @document_shared_list = @document.document_shared_lists.new(document_shared_list_params)
-    authorize @document_shared_list
-    if @document_shared_list.save
+    @shared_document = @document.shared_documents.new(shared_document_params)
+    authorize @shared_document
+    if @shared_document.save
       redirect_to document_path(@document), flash: { validation_message: true, message: "Votre document a bien été ajouté à la liste de partage." }
     else
       flash.now[:errors_attach_document] = true
@@ -12,7 +12,7 @@ class DocumentSharedListsController < ApplicationController
     end
   end
 
-  def document_shared_list_params
-    params.require(:document_shared_list).permit(:shared_list_id)
+  def shared_document_params
+    params.require(:shared_document).permit(:shared_list_id)
   end
 end
--- spec/requests/document_shared_lists_request_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "DocumentSharedLists", type: :request do
-
-end
