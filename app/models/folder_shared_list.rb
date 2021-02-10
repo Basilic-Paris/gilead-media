fe6fa6bf76d6ff6af2dab5f702a0723c384fc0c4
Alice Fabre <51875307+Alice-F@users.noreply.github.com>
2021-02-10 19:40:13 +0100

    Revert "Rename shared folder in folder_shared_list part one"

--- app/models/folder_shared_list.rb
+++
@@ -1,6 +0,0 @@
-class FolderSharedList < ApplicationRecord
-  belongs_to :folder
-  belongs_to :shared_list
-
-  validates :shared_list, uniqueness: { scope: :folder }
-end
--- spec/factories/folder_shared_lists.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :folder_shared_list do
-    folder { nil }
-    shared_list { nil }
-  end
-end
