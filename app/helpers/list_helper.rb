7481d7052b1a1a07bfd24fcdb0c22eaef64b7a0a
Alice Fabre <alice.fabre@hotmail.fr>
2021-02-11 13:59:13 +0100

    add list helper

--- app/helpers/list_helper.rb
+++ app/helpers/list_helper.rb
@@ -1,5 +1,6 @@
 module ListHelper
   def arrange_list(list)
     list.split(/\s{1,}*[,;\/]\s{1,}*|\s{1,}/).uniq
+    # \s{1,}*[,;\/]\s{1,}* (, ; ou / précédé et optionnellement précédé par un ou plusieurs espaces OU un ou plusieurs espaces
   end
 end
