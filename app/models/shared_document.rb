508a87fcd982916409961ddf2edc48f4093a0d45
Alice Fabre <alice.fabre@hotmail.fr>
2021-02-12 16:12:57 +0100

    add validation in shared_document model on contacts presence

--- app/models/shared_document.rb
+++ app/models/shared_document.rb
@@ -5,4 +5,5 @@ class SharedDocument < ApplicationRecord
   has_many :contacts, as: :contactable
 
   validates :code, presence: true, uniqueness: true, length: { is: 16 }
+  validates :contacts, presence: true
 end
