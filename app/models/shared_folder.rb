621a4ebc5a5c22ea4af62a521cae70ec2c7d1cd7
Alice Fabre <alice.fabre@hotmail.fr>
2021-02-12 18:49:22 +0100

    add aasm_state to shared_folders

--- app/models/shared_folder.rb
+++ app/models/shared_folder.rb
@@ -1,4 +1,6 @@
 class SharedFolder < ApplicationRecord
+  include SharedFolderStateMachine
+
   belongs_to :folder
 
   validates :shared_list, uniqueness: { scope: :folder }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_12_174101) do
+ActiveRecord::Schema.define(version: 2021_02_12_174641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 2021_02_12_174101) do
     t.bigint "folder_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "aasm_state"
     t.index ["folder_id"], name: "index_shared_folders_on_folder_id"
   end
 
