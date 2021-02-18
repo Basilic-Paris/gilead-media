45682e132c2b2b434f755dfb65b8c66f06968bdb
Alice Fabre <alice.fabre@hotmail.fr>
2021-02-18 10:35:01 +0100

    add different postmark api keys depending on environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,11 @@
 Rails.application.configure do
   # config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.delivery_method = :postmark
-  # config.action_mailer.postmark_settings = {
-  #   api_token: Rails.application.credentials.postmark_api_token
-  # }
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = {
+    api_token: Rails.application.credentials.dig(:postmark, :dev, :postmark_api_token)
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
