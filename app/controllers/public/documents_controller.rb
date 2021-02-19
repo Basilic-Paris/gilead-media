391a0abcdcab7c5c994a6ea815641659f03e5890
Alice Fabre <alice.fabre@hotmail.fr>
2021-02-19 14:56:53 +0100

    use shared_object to use it intead of shared_list in documents/folders_controller and associated views

--- app/controllers/public/documents_controller.rb
+++ app/controllers/public/documents_controller.rb
@@ -1,7 +1,7 @@
 class Public::DocumentsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
   skip_after_action :verify_authorized, only: :show
-  before_action :find_shared_list, :find_document, only: :show
+  before_action :find_shared_object, :find_document, only: :show
   layout "public"
 
   def show
@@ -9,8 +9,12 @@ class Public::DocumentsController < ApplicationController
 
   private
 
-  def find_shared_list
-    @shared_list = SharedList.find_by(code: params[:shared_list_code])
+  def find_shared_object
+    if params[:shared_list_code].present?
+      @shared_object = SharedList.find_by(code: params[:shared_list_code])
+    elsif params[:shared_folder_code].present?
+      @shared_object = SharedFolder.find_by(code: params[:shared_folder_code])
+    end
   end
 
   def find_document
--- app/controllers/public/folders_controller.rb
+++ app/controllers/public/folders_controller.rb
@@ -1,17 +1,21 @@
 class Public::FoldersController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
   skip_after_action :verify_authorized, only: :show
-  before_action :find_shared_list, :find_folder, only: :show
+  before_action :find_shared_object, :find_folder, only: :show
   layout "public"
 
   def show
-    @documents = DocumentDecorator.decorate_collection(@folder.documents.validated)
+    @documents = DocumentDecorator.decorate_collection(@folder.documents.validated.includes(attachment_attachment: :blob))
   end
 
   private
 
-  def find_shared_list
-    @shared_list = SharedList.find_by(code: params[:shared_list_code])
+  def find_shared_object
+    if params[:shared_list_code].present?
+      @shared_object = SharedList.find_by(code: params[:shared_list_code])
+    elsif params[:shared_folder_code].present?
+      @shared_object = SharedFolder.find_by(code: params[:shared_folder_code])
+    end
   end
 
   def find_folder
