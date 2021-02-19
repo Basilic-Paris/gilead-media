42f9ad20f13a1e3ff145e5c5d51a0899675ef9f9
Alice Fabre <alice.fabre@hotmail.fr>
2021-02-19 12:23:13 +0100

    add class method full_name to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}"
+  end
 end
