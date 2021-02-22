0460dd42f9bd00278d8d04edc7f11590b7f2bc46
Alice Fabre <alice.fabre@hotmail.fr>
2021-02-22 11:52:29 +0100

    add app/policies/public/shared_folder_policy.rb + authorize @shared_folder in app/controllers/public/shared_folders_controller.rb

--- app/controllers/public/shared_folders_controller.rb
+++ app/controllers/public/shared_folders_controller.rb
@@ -1,20 +1,16 @@
 class Public::SharedFoldersController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
-  skip_after_action :verify_authorized, only: :show
   before_action :find_shared_folder, only: :show
   layout "public"
 
   def show
-    if @shared_folder.contacts_added? && (@shared_folder.validity.present? ? Date.today <= @shared_folder.validity : true)
-      @folder = @shared_folder.folder
-    else
-      redirect_to root_path
-    end
+    @folder = @shared_folder.folder
   end
 
   private
 
   def find_shared_folder
     @shared_folder = SharedFolder.find_by(code: params[:code])
+    authorize([:public, @shared_folder])
   end
 end
