3f1d865cad2a8a90200a6bfb791d75676f1efa7b
Alice Fabre <alice.fabre@hotmail.fr>
2021-02-22 11:37:06 +0100

    authorize @shared_list in app/controllers/public/shared_lists_controller.rb

--- app/controllers/public/shared_lists_controller.rb
+++ app/controllers/public/shared_lists_controller.rb
@@ -1,21 +1,17 @@
 class Public::SharedListsController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
-  skip_after_action :verify_authorized, only: :show
   before_action :find_shared_list, only: :show
   layout "public"
 
   def show
-    if @shared_list.contacts_added? && (@shared_list.validity.present? ? Date.today <= @shared_list.validity : true)
-      @documents = DocumentDecorator.decorate_collection(@shared_list.documents.validated.includes(attachment_attachment: :blob))
-      @folders = @shared_list.folders.with_validated_documents
-    else
-      redirect_to root_path
-    end
+    @documents = DocumentDecorator.decorate_collection(@shared_list.documents.validated)
+    @folders = @shared_list.folders.with_validated_documents
   end
 
   private
 
   def find_shared_list
     @shared_list = SharedList.find_by(code: params[:code])
+    authorize([:public, @shared_list])
   end
 end
