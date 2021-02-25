e12045432ebd66b38bb592cd30c8f95e10c3208c
Alice Fabre <alice.fabre@hotmail.fr>
2021-02-25 12:04:27 +0100

    add 2 functions in toggle_controller.js

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,12 +1,26 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "moreOptions" ]
+  static targets = [ "moreOptions", "dropdownIcon", "elementToToggle" ]
 
   connect() {
   }
 
   toggle() {
+    this.moreOptionsTarget.classList.toggle('visible');
+    this.moreOptionsTarget.classList.toggle('invisible');
+  }
+
+  toggleClasses() {
+    this.elementToToggleTarget.getAttribute('data-class-to-toggle').split(', ').forEach((className) => {
+      this.elementToToggleTarget.classList.toggle(className)
+    })
+  }
+
+  toggleWithIcon() {
+    this.dropdownIconTarget.classList.toggle('fa-sort-down');
+    this.dropdownIconTarget.classList.toggle('fa-sort-up');
+
     this.moreOptionsTarget.classList.toggle('visible');
     this.moreOptionsTarget.classList.toggle('invisible');
   }
