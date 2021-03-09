0ee3bd87aca625420e605580cb09cbabdb606451
Alice Fabre <alice.fabre@hotmail.fr>
2021-03-09 10:56:31 +0100

    add policy/controller actions/views for add_to_shared_list, attach_to_existing_shared_list, attach_to_new_shared_list

--- app/policies/folder_policy.rb
+++ app/policies/folder_policy.rb
@@ -7,6 +7,18 @@ class FolderPolicy < ApplicationPolicy
     true
   end
 
+  def add_to_shared_list?
+    true
+  end
+
+  def attach_to_new_shared_list?
+    user.shared_lists.initial.blank?
+  end
+
+  def attach_to_existing_shared_list?
+    !attach_to_new_shared_list?
+  end
+
   # TO KEEP: initial version to add folders or documents to shared list directly
   # def add_to_shared_list?
   #   true
