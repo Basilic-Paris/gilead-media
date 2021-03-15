7badeba9266c5dd78699e6e271066ca662a9780c
Alice Fabre <alice.fabre@hotmail.fr>
2021-03-15 17:08:28 +0100

    remove reload_after_modal_hide_controller.js

--- app/javascript/controllers/reload_after_modal_hide_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from 'stimulus';
-
-export default class extends Controller {
-  connect() {
-    // $('#validationModal').on('hidden.bs.modal', event => {
-    //   document.location.reload();
-    // })
-  }
-}
