e68a84d51ff15fbc44a1ed2f3d3c5538e481042d
Alice Fabre <alice.fabre@hotmail.fr>
2021-03-17 18:20:09 +0100

    update models shared_document / shared_folder / shared_list / user with custom_mail

--- app/models/shared_document.rb
+++ app/models/shared_document.rb
@@ -5,8 +5,12 @@ class SharedDocument < ApplicationRecord
   belongs_to :document
   belongs_to :user
   has_many :contacts, as: :contactable, dependent: :destroy
+  has_one :custom_mail, as: :mailable, dependent: :destroy
 
   validates :code, presence: true, uniqueness: true, length: { is: 16 }
-  validates :contacts, presence: true
   validates :download, inclusion: { in: [true, false] }
+  validates :contacts, presence: true
+  validates :custom_mail, presence: true
+
+  accepts_nested_attributes_for :custom_mail
 end
--- app/models/shared_list.rb
+++ app/models/shared_list.rb
@@ -2,17 +2,29 @@ class SharedList < ApplicationRecord
   include SharedListStateMachine
   include ContactHelper
 
+  attr_accessor :validate_custom_mail
+
   belongs_to :user
   has_many :document_shared_lists, dependent: :destroy
   has_many :documents, through: :document_shared_lists
   has_many :folder_shared_lists, dependent: :destroy
   has_many :folders, through: :folder_shared_lists
   has_many :contacts, as: :contactable, dependent: :destroy
+  has_one :custom_mail, as: :mailable, dependent: :destroy
 
   validates :title, presence: true#, uniqueness: { scope: :user }
   validates :code, presence: true, uniqueness: true, length: { is: 16 }
-  validates :contacts, presence: true, on: :update
   validates :download, inclusion: { in: [true, false] }
+  validates :contacts, presence: true, on: :update
+  validates :custom_mail, presence: true, if: :validate_custom_mail?
+
+  accepts_nested_attributes_for :custom_mail
 
   scope :ordered_by_title, -> { order('lower(title)') }
+
+  private
+
+  def validate_custom_mail?
+    validate_custom_mail == true
+  end
 end
