08d6989d75e50d7626b26734fc611a290401f4c6
Alice Fabre <alice.fabre@hotmail.fr>
2021-03-17 18:32:03 +0100

    display custom mail in contact mailer / notify_contacts

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,7 @@ class ContactMailer < ApplicationMailer
   def notify_contacts(contact)
     @contact = contact
     @user = @contact.contactable.user
+    @custom_mail = @contact.contactable.custom_mail
     mail(to: @contact.email, subject: "#{@user.full_name} souhaite partager des documents avec vous")
   end
 end
