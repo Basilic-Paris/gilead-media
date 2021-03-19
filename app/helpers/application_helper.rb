afb8b7824184f16febeace167d47cf5f5757da24
Alice Fabre <alice.fabre@hotmail.fr>
2021-03-19 09:04:08 +0100

    disable turbolinks cache for all

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def turbolinks_cache_control_meta_tag
-    tag :meta, name: 'turbolinks-cache-control', content: @turbolinks_cache_control || 'cache'
+    tag :meta, name: 'turbolinks-cache-control', content: @turbolinks_cache_control || 'no-cache'
   end
 end
