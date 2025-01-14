a0813a769e6c76563331241e63d3bc6f8b96a77e
Alice Fabre <alice.fabre@hotmail.fr>
2025-01-14 17:15:30 +0100

    use importmap instead of webpack

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link popper.js
+//= link bootstrap.min.js
+//= link jquery.min.js
---
+++ app/javascript/application.js
@@ -0,0 +1,76 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "@popperjs/core"
+import "jquery"
+import "bootstrap"
+import "./plugins/jquery"
+
+// document.addEventListener('turbo:load', () => {
+//   // Call your functions here, e.g:
+//   // initSelect2();
+//   initComponents();
+// });
+
+Turbo.StreamActions.open_modal = function () {
+  // console.log(this)
+  // console.log(this.action)
+  // console.log(this.target)
+  // $(`.modal#${this.target}`).modal("show")
+  // $(`${this.target}`).modal("show")
+  $(this.target).modal("show")
+};
+
+Turbo.StreamActions.close_modal = function () {
+  // console.log(this)
+  // console.log(this.action)
+  // console.log(this.target)
+  // $(`.modal#${this.target}`).modal("hide")
+  // $(`${this.target}`).modal("hide")
+  $(this.target).modal("hide")
+};
+
+Turbo.StreamActions.remove_validation_errors = function() {
+  $('.invalid-feedback').html('');
+  $('.is-valid').removeClass('is-valid');
+  $('.is-invalid').removeClass('is-invalid');
+}
+
+Turbo.StreamActions.set_validation_errors = function () {
+  const attributes = JSON.parse(this.getAttribute("attributes"))
+  $(attributes["elementId"]).addClass('is-invalid ')
+  $(attributes["invalidFeedbackElement"]).html(
+    attributes["message"]
+  );
+}
+
+Turbo.StreamActions.replace_element_attribute = function () {
+  const attributes = JSON.parse(this.getAttribute("attributes"))
+  $(attributes["element"]).attr(attributes["attribute"], attributes["newAttributeValue"])
+}
+
+Turbo.StreamActions.remove_parent = function () {
+  document.querySelector(this.target).parentNode.remove()
+}
+
+Turbo.StreamActions.remove_element = function () {
+  document.querySelector(this.target).remove()
+}
+
+Turbo.StreamActions.redirect = function () {
+  Turbo.visit(this.target);
+};
+
+Turbo.setConfirmMethod((message, element) => {
+  let dialog = document.getElementById("turbo-confirm")
+  dialog.querySelector("p").outerHTML = message
+  dialog.showModal()
+  return new Promise((resolve, reject) => {
+    dialog.addEventListener('click', (event) => {
+      event.currentTarget === event.target && dialog.close();
+    })
+    dialog.addEventListener("close", () => {
+      resolve(dialog.returnValue === "confirm")
+    }, { once: true })
+  })
+})
