963b5fd9b7ba4e3dd14d3fcb2818d890a21c3c6c
Alice Fabre <alice.fabre@hotmail.fr>
2025-01-15 09:35:23 +0100

    remove TurbolinksCacheControl from app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
-  include TurbolinksCacheControl
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
