10ea1ad0c18d5fa8b51a4b2dffaa46a1ff0aa97c
Alice Fabre <alice.fabre@hotmail.fr>
2025-01-16 11:46:12 +0100

    correct app/javascript/controllers/flatpickr_controller.js

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -7,13 +7,15 @@ export default class extends Controller {
   ]
 
   connect() {
-    flatpickr(this.dateSelectionTarget, {
-      // locale: "fr", # replace by flatpickr.localize(French)
-      // mode: "range",
-      minDate: 'today',
-      altInput: true,
-      altFormat: "D j F Y",
-      dateFormat: "Y-m-d"
-    });
+    if (this.hasDateSelectionTarget) {
+      flatpickr(this.dateSelectionTarget, {
+        // locale: "fr", # replace by flatpickr.localize(French)
+        // mode: "range",
+        minDate: 'today',
+        altInput: true,
+        altFormat: "D j F Y",
+        dateFormat: "Y-m-d"
+      });
+    }
   }
 }
